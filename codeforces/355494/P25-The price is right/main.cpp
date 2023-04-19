5183825980f0aa5d6e3cc7c403c894ceb2a5e393
Ahsan <ahsan.aasim@gmail.com>
2023-04-20 00:06:09 +0600

    solved P25-The price is right

---
+++ codeforces/355494/P25-The price is right/main.cpp
@@ -0,0 +1,24 @@
+// https://codeforces.com/group/yg7WhsFsAp/contest/355494/problem/P25
+// https://codeforces.com/group/yg7WhsFsAp/contest/355494/problem/P24
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 6;
+    cin >> n;
+    int a[n];
+
+    int min = 9999999;
+    int index = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        if (min > a[i])
+        {
+            min = a[i];
+            index = i;
+        }
+    }
+    cout << index << endl;
+    return 0;
+}
