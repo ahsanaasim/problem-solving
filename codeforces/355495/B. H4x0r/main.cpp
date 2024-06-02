999c080c8dc6eae887f8863d6d45d07f0e892c5f
Ahsan <ahsan.aasim@gmail.com>
2024-06-02 16:21:16 +0600

    solve B. H4x0r problem

---
+++ codeforces/355495/B. H4x0r/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string cipher = "aieos";
+string cipherCode = "41305";
+
+int indexOfChar(char c)
+{
+    int index = cipher.find(c);
+
+    if (index != string::npos)
+    {
+        return index;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int main()
+{
+    string input = "aeios", output = "";
+    cin >> input;
+    for (int i = 0; i < input.size(); i++)
+    {
+        int index = indexOfChar(input[i]);
+        // cout << index << endl;
+        if (index >= 0)
+        {
+            output += cipherCode[index];
+        }
+        else
+        {
+            output += input[i];
+        }
+    }
+    cout << output << endl;
+    return 0;
+}
